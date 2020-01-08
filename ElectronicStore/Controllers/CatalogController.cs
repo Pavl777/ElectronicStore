1071324e7a42b6091b4cc8fefce719c83e58aa9c
Pavel <pavelsukhina.777@gmail.com>
2020-01-08 21:22:39 +0200

    Content upload

---
+++ ElectronicStore/Controllers/CatalogController.cs
@@ -0,0 +1,30 @@
+﻿using ElectronicStore.Models.Catalog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElectronicStore.Controllers
+{
+    public class CatalogController : Controller
+    {
+
+        // GET: Catalog
+        public ActionResult Index()
+        {
+            Product product1 = new Product {
+                ID = 1,
+                Name = "TTel",
+                Img = "https://image.freepik.com/free-vector/_1412-16.jpg"
+            };
+            Product product2 = new Product { 
+                ID = 2,
+                Name = "Fridge",
+                Img = "http://grigorov.pro/wp-content/uploads/2015/06/Holodil-nik.jpg"
+            };
+            List<Product> products = new List<Product> { product1, product2 };
+            return View(products);
+        }
+    }
+}
---
+++ ElectronicStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ElectronicStore.Startup))]
+namespace ElectronicStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
